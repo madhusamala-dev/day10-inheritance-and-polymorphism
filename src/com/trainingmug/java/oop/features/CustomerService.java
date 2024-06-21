36c3a64d75f8330a1de85a6fe7c2ba0f97abc4ba
Madhu Samala <madhu@trainingmug.com>
2024-06-21 19:51:04 +0530

    working with inheritance and polymorphism

---
+++ src/com/trainingmug/java/oop/features/CustomerService.java
@@ -0,0 +1,19 @@
+package com.trainingmug.java.oop.features;
+
+public class CustomerService {
+
+    //business logics
+    public void displayCustomerDetails(Customer customer){
+        try{
+            // code
+        }  catch (Excpetion e){
+            // Exception handling code
+        } finally {
+            // resource cleaning code
+        }
+
+        customer.displayCustomerDetails();
+    }
+
+    
+}
